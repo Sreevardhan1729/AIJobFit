68e27d1b4a5ef465305f7d8e411a4a255563898f
Sreevardhan1729 <sreevardhan1729@gmail.com>
2025-03-24 13:50:46 +0530

    updated-link

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = 'http://127.0.0.1:5000';
+const API_URL = 'https://flask-hello-world-u7ol.onrender.com';
 
 const getJobSkills = async (jobDescription) => {
     try {
